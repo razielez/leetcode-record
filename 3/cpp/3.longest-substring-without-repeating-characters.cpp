47f7aea4185f6b24ef5ffda54e4f7adeb5f34823
baize <linran@live.com>
2020-12-22 19:06:05 +0800

    .

---
+++ 3/cpp/3.longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=3 lang=cpp
+ *
+ * [3] Longest Substring Without Repeating Characters
+ *
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (30.92%)
+ * Likes:    12095
+ * Dislikes: 649
+ * Total Accepted:    1.9M
+ * Total Submissions: 6.1M
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * Given a string s, find the length of the longest substring without repeating
+ * characters.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not
+ * a substring.
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: s = ""
+ * Output: 0
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= s.length <= 5 * 10^4
+ * s consists of English letters, digits, symbols and spaces.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+    }
+};
+// @lc code=end
