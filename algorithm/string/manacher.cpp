114922c5a23625de2ea9ff8660851b09be5bdec8
baize <linran@live.com>
2021-03-09 12:19:10 +0800

    .

---
+++ algorithm/string/manacher.cpp
@@ -0,0 +1,10 @@
+#include <stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+	string test_str = "10001testtest10101"
+
+	return 0;
+}
---
+++ src/main/java/com/lin/p4/Solution4.java
@@ -0,0 +1,11 @@
+package com.lin.p4;
+
+public class Solution4 {
+
+  public static void main(String[] args) {
+
+
+  }
+
+
+}
